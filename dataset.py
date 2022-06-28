ec54ce80884292ae8d6d50a06b2a132ac2847b3f
lanlinbuaa <lanlinjnc@buaa.edu.cn>
2022-06-28 11:14:07 +0800

    Initial commit

---
+++ dataset.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/6/28 11:10
+# @Author  : lanlin
+
+
+import numpy as np
+import torch
+
+
+
+def input_I(t):
+    pass
+
+
+def input_u(t):
+    pass
+
+
+
+if __name__=="__main__":
+    pass
